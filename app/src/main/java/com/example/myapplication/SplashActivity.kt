47b3d6f318fc5920f618ae4d2c5fe39a4536c170
JosephWijaya34 <92154348+JosephWijaya34@users.noreply.github.com>
2022-06-08 18:23:14 +0700

    AWAL

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myapplication.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var viewBind: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBind=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(viewBind.root)
+
+        //function splash screen
+        showLogin()
+    }
+
+    private fun showLogin(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val die = Intent(this, MainActivity::class.java)
+            die.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(die)
+        }, 3000)
+    }
+}
