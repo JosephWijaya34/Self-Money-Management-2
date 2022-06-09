4e939b26f46589299b657d5463ba55acf373e293
JosephWijaya34 <92154348+JosephWijaya34@users.noreply.github.com>
2022-06-09 12:03:03 +0700

    test2

---
+++ app/src/main/java/com/example/myapplication/test2.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class test2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test2)
+        Toast.makeText("text", "HAHAHAHAHAHA", Toast.LENGTH_LONG).show()
+    }
+}
